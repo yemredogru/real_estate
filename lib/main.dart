38d021b04f9c9ea043fc9a4dac3d4da447791174
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2022-07-31 23:51:06 +0300

    home_page continue

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+//https://dribbble.com/shots/18931082-Real-Estate-Mobile-App
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -103,7 +105,249 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Text(
+                  "Categories",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                Spacer(),
+                Text("View All"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    height: MediaQuery.of(context).size.height * 0.04,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xff1f005c),
+                          Color(0xff5b0060),
+                          Color(0xff870160),
+                          Color(0xffac255e),
+                          Color(0xffca485c),
+                          Color(0xffe16b5c),
+                          Color(0xfff39060),
+                          Color(0xffffb56b),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(
+                          Icons.home,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "House",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xff1f005c),
+                            Color(0xff5b0060),
+                            Color(0xff870160),
+                            Color(0xffac255e),
+                            Color(0xffca485c),
+                            Color(0xffe16b5c),
+                            Color(0xfff39060),
+                            Color(0xffffb56b),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.hotel,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Hotel",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xff1f005c),
+                            Color(0xff5b0060),
+                            Color(0xff870160),
+                            Color(0xffac255e),
+                            Color(0xffca485c),
+                            Color(0xffe16b5c),
+                            Color(0xfff39060),
+                            Color(0xffffb56b),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.apartment,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Apartment",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: MediaQuery.of(context).size.height * 0.04,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xff1f005c),
+                            Color(0xff5b0060),
+                            Color(0xff870160),
+                            Color(0xffac255e),
+                            Color(0xffca485c),
+                            Color(0xffe16b5c),
+                            Color(0xfff39060),
+                            Color(0xffffb56b),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.villa,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Villa",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 16.0,
+              left: 16.0,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  "Top Pick,s",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  color: Colors.grey,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(32.0),
+                            child: Image.asset(
+                              'assets/images/modern.jpg',
+                              width: MediaQuery.of(context).size.width * 0.65,
+                              height: MediaQuery.of(context).size.height * 0.3,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Modern Family House"),
+                        Row(
+                          children: [
+                            Icon(Icons.gps_fixed_outlined),
+                            Text("168 Old town road")
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text("886/Month"),
+                            Spacer(),
+                            Icon(Icons.star),
+                            Text("4.9")
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
